354d9e512f36b3377a90df9fa8558552135065c0
andretan <andretan@stud.ntnu.no>
2023-10-01 16:06:35 +0200

    added gradient to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,19 +6,26 @@ import '../assets/Header.css'
 function Header() {
 
     return ( 
-        <div className="header">
-            <div className="homepage">
-                <Link to="/"><img src={A_logo} width="60vh"></img></Link>
+        <>
+            <div className="header">
+                <div className="homepage">
+                    <Link to="/"><img src={A_logo} width="60vh"></img></Link>
+                </div>
+                <div className="sections">
+                    <Link to="/about">About</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/expertise">Expertise</Link>
+                </div>
+                <div className="getintouchWrapper">
+                    <Link to="/contact"><button className="getintouch">Get in touch</button></Link>
+                </div>
             </div>
-            <div className="sections">
-                <Link to="/about">About</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/expertise">Expertise</Link>
+            <div className="backgroundContainer">
+                <div className="backgroundGrid" tabIndex={0} style={{display: 'contents'}}>
+                    <div className="backgroundHeader"></div>
+                </div>
             </div>
-            <div className="getintouchWrapper">
-                <Link to="/contact"><button className="getintouch">Get in touch</button></Link>
-            </div>
-        </div>
+        </>
     )
 }
 
