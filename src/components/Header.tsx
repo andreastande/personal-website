96913a1ee0269dbb6a36552f4093f0d22e687bf9
andretan <andretan@stud.ntnu.no>
2023-10-12 05:11:14 +0200

    improved styling of header and burger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,26 @@ import A_logo from '../assets/images/A_logo.png'
 import '../assets/Header.css'
 import { useState, useEffect } from 'react';
 import Hamburger_icon from '../assets/images/Hamburger_icon.png'
+import Close_Hamburger_icon from '../assets/images/Close_Hamburger_icon.png'
 
 
 function Header() {
 
     const [fadeIn, setFadeIn] = useState(false);
     const [isHomepage] = useState(window.location.pathname === '/');
+    const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
         setFadeIn(true);
-      }, []);
+    }, []);
+
+      function toggleMenu() {
+        setMenuOpen(!menuOpen);
+      }
 
     return ( 
         <>
-            <div className="header">
+            <div className={`header ${menuOpen ? 'menu-open' : ''}`}>
                 <div className="logo">
                     <Link to="/"><img src={A_logo} width="70vw"></img></Link>
                 </div>
@@ -28,8 +34,8 @@ function Header() {
                 <div className="getintouchWrapper">
                     <Link to="/contact"><button className="getintouch">Get in touch</button></Link>
                 </div>
-                <div className="burger-menu">
-                    <img className="burger-icon" src={Hamburger_icon} width="50vw"></img>
+                <div className="burger-menu" onClick={toggleMenu}>
+                    <img className="burger-icon" src={!menuOpen ? Hamburger_icon : Close_Hamburger_icon} width="50vw"></img>
                 </div>
             </div>
             <div className="backgroundContainer">
