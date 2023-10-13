ef12341f47f41a15900a3e28771721a1d93d52cc
andretan <andretan@stud.ntnu.no>
2023-10-13 14:13:24 +0200

    made header sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import A_logo from '../assets/images/A_logo.png'
 import '../assets/Header.css'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Hamburger_icon from '../assets/images/Hamburger_icon.png'
 import Close_Hamburger_icon from '../assets/images/Close_Hamburger_icon.png'
 
@@ -11,18 +11,36 @@ function Header() {
     const [fadeIn, setFadeIn] = useState(false);
     const [isHomepage] = useState(window.location.pathname === '/');
     const [menuOpen, setMenuOpen] = useState(false);
+    const [hasScrolled, setHasScrolled] = useState(false);
+    const hasScrolledRef = useRef(false);
 
     useEffect(() => {
         setFadeIn(true);
+        
+        function handleScroll() {
+            if (window.scrollY > 0) {
+                hasScrolledRef.current = true;
+                setHasScrolled(true);
+            } else {
+                hasScrolledRef.current = false;
+                setHasScrolled(false);
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
     }, []);
 
-      function toggleMenu() {
+    function toggleMenu() {
         setMenuOpen(!menuOpen);
-      }
+    }
 
     return ( 
         <>
-            <div className={`header ${menuOpen ? 'menu-open' : ''}`}>
+            <div className={`header ${menuOpen || hasScrolled ? 'menu-open' : ''}`}>
                 <div className="logo">
                     <Link to="/"><img src={A_logo} width="70vw"></img></Link>
                 </div>
@@ -45,7 +63,10 @@ function Header() {
                 </div>
             </div>
             {menuOpen && (
-                <div className="backdrop-blur"></div>
+                <div className="backdrop-blur header-and-menu"></div>
+            )}
+            {!menuOpen && hasScrolled && (
+                <div className="backdrop-blur header"></div>
             )}
             <div className="backgroundContainer">
                 <div className="backgroundGrid" tabIndex={0} style={{display: 'contents'}}>
