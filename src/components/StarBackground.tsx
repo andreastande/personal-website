4a60e3ffc4afbf6009bd26ed92b2d7174b05ba42
andretan <andretan@stud.ntnu.no>
2023-10-16 12:43:52 +0200

    stars are now perfect

--- src/components/StarBackground.tsx
+++ src/components/StarBackground.tsx
@@ -20,81 +20,235 @@ const StarBackground = () => {
     return (
         <div className="star-particles">
             {showParticles && (
-                <Particles height="90vh" id="stars" init={particlesInit} options={{
+                <Particles height="90vh" id="stars1" init={particlesInit} options={{
                     "particles": {
-                    "number": {
-                        "value": 200,
-                        "density": {
-                        "enable": true,
-                        "value_area": 800
-                        }
-                    },
-                    "color": {
-                        "value": "#ffffff"
-                    },
-                    "shape": {
-                        "type": "circle",
-                        "stroke": {
-                        "width": 0,
-                        "color": "#000000"
+                        "number": {
+                            "value": 200,
+                            "density": {
+                                "enable": true,
+                                "value_area": 800
+                            }
                         },
-                        "polygon": {
-                        "nb_sides": 3
+                        "color": {
+                            "value": "#ffffff"
                         },
-                        "image": {
-                        "src": "img/github.svg",
-                        "width": 100,
-                        "height": 100
+                        "shape": {
+                            "type": "circle",
+                            "stroke": {
+                                "width": 0,
+                                "color": "#000000"
+                            },
+                            "polygon": {
+                                "nb_sides": 3
+                            },
+                            "image": {
+                                "src": "img/github.svg",
+                                "width": 100,
+                                "height": 100
+                            }
+                        },
+                        "opacity": {
+                            "value": 0.8,
+                            "random": true,
+                            "anim": {
+                                "enable": false,
+                                "speed": 1,
+                                "opacity_min": 0.1,
+                                "sync": false
+                            }
+                        },
+                        "size": {
+                            "value": 1,
+                            "random": true,
+                            "anim": {
+                            "enable": false,
+                            "speed": 40,
+                            "size_min": 0.1,
+                            "sync": false
+                            }
+                        },
+                        "line_linked": {
+                            "enable": false,
+                            "distance": 150,
+                            "color": "#ffffff",
+                            "opacity": 0.4,
+                            "width": 1
+                        },
+                        "move": {
+                            "enable": true,
+                            "speed": 0.3,
+                            "direction": "left",
+                            "random": true,
+                            "straight": true,
+                            "out_mode": "out",
+                            "bounce": false,
+                            "attract": {
+                                "enable": false,
+                                "rotateX": 600,
+                                "rotateY": 1200
+                            }
                         }
-                    },
-                    "opacity": {
-                        "value": 0.8,
-                        "random": true,
-                        "anim": {
-                        "enable": false,
-                        "speed": 1,
-                        "opacity_min": 0.1,
-                        "sync": false
-                        }
-                    },
-                    "size": {
-                        "value": 1,
-                        "random": true,
-                        "anim": {
-                        "enable": false,
-                        "speed": 40,
-                        "size_min": 0.1,
-                        "sync": false
-                        }
-                    },
-                    "line_linked": {
-                        "enable": false,
-                        "distance": 150,
-                        "color": "#ffffff",
-                        "opacity": 0.4,
-                        "width": 1
-                    },
-                    "move": {
-                        "enable": true,
-                        "speed": 0.3,
-                        "direction": "left",
-                        "random": true,
-                        "straight": true,
-                        "out_mode": "out",
-                        "bounce": false,
-                        "attract": {
-                        "enable": false,
-                        "rotateX": 600,
-                        "rotateY": 1200
-                        }
-                    }
                     },
                     "style": {
-                        "height": "80vh"
+                        "height": "55vh"   
                     },
                     "retina_detect": true
                 }} />
             )}
+            {showParticles && (
+                <Particles id="stars2" init={particlesInit} options={{
+                    "particles": {
+                        "number": {
+                            "value": 180,
+                            "density": {
+                                "enable": true,
+                                "value_area": 800
+                            }
+                        },
+                        "color": {
+                            "value": "#ffffff"
+                        },
+                        "shape": {
+                            "type": "circle",
+                            "stroke": {
+                                "width": 0,
+                                "color": "#000000"
+                            },
+                            "polygon": {
+                                "nb_sides": 3
+                            },
+                            "image": {
+                                "src": "img/github.svg",
+                                "width": 100,
+                                "height": 100
+                            }
+                        },
+                        "opacity": {
+                            "value": 0.5,
+                            "random": true,
+                            "anim": {
+                                "enable": false,
+                                "speed": 1,
+                                "opacity_min": 0.1,
+                                "sync": false
+                            }
+                        },
+                        "size": {
+                            "value": 1,
+                            "random": true,
+                            "anim": {
+                            "enable": false,
+                            "speed": 40,
+                            "size_min": 0.1,
+                            "sync": false
+                            }
+                        },
+                        "line_linked": {
+                            "enable": false,
+                            "distance": 150,
+                            "color": "#ffffff",
+                            "opacity": 0.4,
+                            "width": 1
+                        },
+                        "move": {
+                            "enable": true,
+                            "speed": 0.3,
+                            "direction": "left",
+                            "random": true,
+                            "straight": true,
+                            "out_mode": "out",
+                            "bounce": false,
+                            "attract": {
+                                "enable": false,
+                                "rotateX": 600,
+                                "rotateY": 1200
+                            }
+                        }
+                    },
+                    "style": {
+                        "height": "15vh",
+                        "margin": "55vh 0px 0px 0px"  
+                    },
+                    "retina_detect": true
+                }} />
+            )}
+            {showParticles && (
+                    <Particles id="stars3" init={particlesInit} options={{
+                        "particles": {
+                            "number": {
+                                "value": 180,
+                                "density": {
+                                    "enable": true,
+                                    "value_area": 800
+                                }
+                            },
+                            "color": {
+                                "value": "#ffffff"
+                            },
+                            "shape": {
+                                "type": "circle",
+                                "stroke": {
+                                    "width": 0,
+                                    "color": "#000000"
+                                },
+                                "polygon": {
+                                    "nb_sides": 3
+                                },
+                                "image": {
+                                    "src": "img/github.svg",
+                                    "width": 100,
+                                    "height": 100
+                                }
+                            },
+                            "opacity": {
+                                "value": 0.2,
+                                "random": true,
+                                "anim": {
+                                    "enable": false,
+                                    "speed": 1,
+                                    "opacity_min": 0.1,
+                                    "sync": false
+                                }
+                            },
+                            "size": {
+                                "value": 1,
+                                "random": true,
+                                "anim": {
+                                "enable": false,
+                                "speed": 40,
+                                "size_min": 0.1,
+                                "sync": false
+                                }
+                            },
+                            "line_linked": {
+                                "enable": false,
+                                "distance": 150,
+                                "color": "#ffffff",
+                                "opacity": 0.4,
+                                "width": 1
+                            },
+                            "move": {
+                                "enable": true,
+                                "speed": 0.3,
+                                "direction": "left",
+                                "random": true,
+                                "straight": true,
+                                "out_mode": "out",
+                                "bounce": false,
+                                "attract": {
+                                    "enable": false,
+                                    "rotateX": 600,
+                                    "rotateY": 1200
+                                }
+                            }
+                        },
+                        "style": {
+                            "height": "10vh",
+                            "margin": "70vh 0px 0px 0px"  
+                        },
+                        "retina_detect": true
+                    }} />
+                )}
         </div>
     );
 };
