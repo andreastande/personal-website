3c61d5a0b0611ef542edf06ad5f542fe42809dd2
andretan <andretan@stud.ntnu.no>
2023-10-18 11:37:17 +0200

    added a title to homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,17 +1,17 @@
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import StarBackground from '../components/StarBackground';
+import '../assets/Homepage.css'
 
 const Homepage = () => {
     return (
         <div>
             <Header />
             <main className="mainContent">
-                <h1>Hello from homepage</h1>
+                <div className="landing-section">
+                    <h1 className="page-title">Velkommen til min hjemside</h1>
+                </div>
                 <p>This is a basic homepage component.</p>
-                <button>
-                    <Link to="/about">Go to Aboutpage</Link>
-                </button>
                 <StarBackground />
             </main>
         </div>
