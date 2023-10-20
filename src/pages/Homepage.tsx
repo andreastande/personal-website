6c84b555028e20db3f8e1eed85a7ec96d3fefbbe
andretan <andretan@stud.ntnu.no>
2023-10-20 13:09:45 +0200

    commented out buggy code

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -23,26 +23,28 @@ const Homepage = () => {
                     <section className="grid-container">
                         <Link to="/about" className="grid-row-span-2">
                             <div style={{position: 'relative'}}>
-                                <article className="grid-box-1" onMouseOver={() => setShowArrow1(true)} onMouseOut={() => setShowArrow1(false)}>
+                                <article className="grid-box-1" /* onMouseOver={() => setShowArrow1(true)} onMouseOut={() => setShowArrow1(false)} */ >
                                     <img src={ProfilePicture}></img>
                                     <p>Mitt navn er Andreas Tande, jeg er 22 år og studerer Datateknologi ved NTNU. For øyeblikket går jeg mitt tredje år på dette studiet.</p>
                                     <p id="additional-text-1">Klikk for å lese mer!</p>
                                 </article>
-                                {showArrow1 && (
-                                    <div className="showMore" style={{position: 'absolute', left: '-40px', bottom: '-60px', display: 'flex', flexDirection: 'column'}}>
-                                        <img src={ArrowRight} width="60px"></img>
-                                        <p style={{margin: '0'}}>Klikk for mer info</p>
-                                    </div>
-                                )}
+                                {/* 
+                                    {showArrow1 && (
+                                        <div className="showMore" style={{position: 'absolute', left: '-40px', bottom: '-60px', display: 'flex', flexDirection: 'column'}}>
+                                            <img src={ArrowRight} width="60px"></img>
+                                            <p style={{margin: '0'}}>Klikk for mer info</p>
+                                        </div>
+                                    )}
+                                */}
                             </div>
                         </Link>
                         <Link to="/projects" className="grid-row-span-1">
-                            <article className="grid-box-2" onMouseOver={() => setShowArrow2(true)} onMouseOut={() => setShowArrow2(false)}>
+                            <article className="grid-box-2" /* onMouseOver={() => setShowArrow2(true)} onMouseOut={() => setShowArrow2(false)} */ >
                             
                             </article>
                         </Link>
                         <Link to="/expertise" className="grid-row-span-1">
-                            <article className="grid-box-3" onMouseOver={() => setShowArrow3(true)} onMouseOut={() => setShowArrow3(false)}>
+                            <article className="grid-box-3" /* onMouseOver={() => setShowArrow3(true)} onMouseOut={() => setShowArrow3(false)} */ >
                                 
                             </article>
                         </Link>
