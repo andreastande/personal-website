d3bc11451cc731aecaee9c7812fd4a5e4eb18ef3
andretan <andretan@stud.ntnu.no>
2023-10-21 21:40:41 +0200

    made grid-elements have a height of min-content

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -23,35 +23,37 @@ const Homepage = () => {
                     <p className="subheading">
                         Her kan du få vite mer om meg, og oppdage prosjekter jeg har bidratt i eller utviklet selv. 
                     </p>
-                    <section className="grid-container">
-                        <Link to="/about" className="grid-row-span-2">
-                            <div style={{position: 'relative'}}>
-                                <article className="grid-box-1" /* onMouseOver={() => setShowArrow1(true)} onMouseOut={() => setShowArrow1(false)} */ >
-                                    <img src={ProfilePicture}></img>
-                                    <p>Mitt navn er Andreas Tande, jeg er 22 år og studerer Datateknologi ved NTNU. For øyeblikket går jeg mitt tredje år her.</p>
-                                    <p id="additional-text-1">Klikk for å lese mer!</p>
-                                </article>
-                                {/* 
-                                    {showArrow1 && (
-                                        <div className="showMore" style={{position: 'absolute', left: '-40px', bottom: '-60px', display: 'flex', flexDirection: 'column'}}>
-                                            <img src={ArrowRight} width="60px"></img>
-                                            <p style={{margin: '0'}}>Klikk for mer info</p>
-                                        </div>
-                                    )}
-                                */}
-                            </div>
-                        </Link>
-                        <Link to="/projects" className="grid-row-span-1">
-                            <article className="grid-box-2" /* onMouseOver={() => setShowArrow2(true)} onMouseOut={() => setShowArrow2(false)} */ >
-                            
-                            </article>
-                        </Link>
-                        <Link to="/expertise" className="grid-row-span-1">
-                            <article className="grid-box-3" /* onMouseOver={() => setShowArrow3(true)} onMouseOut={() => setShowArrow3(false)} */ >
+                    <div className="grid-container-wrapper">
+                        <section className="grid-container">
+                            <Link to="/about" className="grid-row-span-2">
+                                <div style={{position: 'relative'}}>
+                                    <article className="grid-box-1" /* onMouseOver={() => setShowArrow1(true)} onMouseOut={() => setShowArrow1(false)} */ >
+                                        <img src={ProfilePicture}></img>
+                                        <p>Mitt navn er Andreas Tande, jeg er 22 år og studerer Datateknologi ved NTNU. For øyeblikket er jeg på mitt tredje år.</p>
+                                        <p id="additional-text-1">Klikk for å lese mer!</p>
+                                    </article>
+                                    {/* 
+                                        {showArrow1 && (
+                                            <div className="showMore" style={{position: 'absolute', left: '-40px', bottom: '-60px', display: 'flex', flexDirection: 'column'}}>
+                                                <img src={ArrowRight} width="60px"></img>
+                                                <p style={{margin: '0'}}>Klikk for mer info</p>
+                                            </div>
+                                        )}
+                                    */}
+                                </div>
+                            </Link>
+                            <Link to="/projects" className="grid-row-span-1">
+                                <article className="grid-box-2" /* onMouseOver={() => setShowArrow2(true)} onMouseOut={() => setShowArrow2(false)} */ >
                                 
-                            </article>
-                        </Link>
-                    </section>
+                                </article>
+                            </Link>
+                            <Link to="/expertise" className="grid-row-span-1">
+                                <article className="grid-box-3" /* onMouseOver={() => setShowArrow3(true)} onMouseOut={() => setShowArrow3(false)} */ >
+                                    
+                                </article>
+                            </Link>
+                        </section>
+                    </div>
                 </div>
                 <StarBackground />
             </main>
