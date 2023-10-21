6f880b13551d970cd96efc94554a3a1dfc14533e
andretan <andretan@stud.ntnu.no>
2023-10-22 00:03:42 +0200

    fixed bug with starBackground changing randomly

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -5,12 +5,48 @@ import '../assets/Homepage.css'
 import { Link } from 'react-router-dom';
 import ProfilePicture from '../assets/images/Profilbilde.png'
 import ArrowRight from '../assets/images/arrow-right.png'
+import PointerRight from '../assets/images/cursor-pointer-right.png'
 
-const Homepage = () => {
+const HomepageGrid = () => {
     const [showArrow1, setShowArrow1] = useState(false);
     const [showArrow2, setShowArrow2] = useState(false);
     const [showArrow3, setShowArrow3] = useState(false);
 
+    return (
+        <section className="grid-container">
+            <Link to="/about" className="grid-row-span-2">
+                <div style={{position: 'relative'}}>
+                    <article className="grid-box-1" onMouseOver={() => setShowArrow1(true)} onMouseOut={() => setShowArrow1(false)}>
+                        <img src={ProfilePicture}></img>
+                        <p>Mitt navn er Andreas Tande, jeg er 22 år og studerer Datateknologi ved NTNU. For øyeblikket er jeg på mitt tredje år.</p>
+                        <p id="additional-text-1">Klikk for å lese mer!</p>
+                    </article>
+                    {showArrow1 && (
+                        <div className="showMore" style={{position: 'absolute', left: '-40px', bottom: '-40px'}}>
+                            <img src={PointerRight} width="60px"></img>
+                            
+                        </div>
+                    )}
+                    
+                </div>
+            </Link>
+            
+            <Link to="/projects" className="grid-row-span-1">
+                <article className="grid-box-2" /* onMouseOver={() => setShowArrow2(true)} onMouseOut={() => setShowArrow2(false)} */ >
+                
+                </article>
+            </Link>
+            <Link to="/expertise" className="grid-row-span-1">
+                <article className="grid-box-3" /* onMouseOver={() => setShowArrow3(true)} onMouseOut={() => setShowArrow3(false)} */ >
+                    
+                </article>
+            </Link>
+        </section>
+    )
+}
+
+const Homepage = () => {
+    
     return (
         <div>
             <Header />
@@ -24,35 +60,7 @@ const Homepage = () => {
                         Her kan du få vite mer om meg, og oppdage prosjekter jeg har bidratt i eller utviklet selv. 
                     </p>
                     <div className="grid-container-wrapper">
-                        <section className="grid-container">
-                            <Link to="/about" className="grid-row-span-2">
-                                <div style={{position: 'relative'}}>
-                                    <article className="grid-box-1" /* onMouseOver={() => setShowArrow1(true)} onMouseOut={() => setShowArrow1(false)} */ >
-                                        <img src={ProfilePicture}></img>
-                                        <p>Mitt navn er Andreas Tande, jeg er 22 år og studerer Datateknologi ved NTNU. For øyeblikket er jeg på mitt tredje år.</p>
-                                        <p id="additional-text-1">Klikk for å lese mer!</p>
-                                    </article>
-                                    {/* 
-                                        {showArrow1 && (
-                                            <div className="showMore" style={{position: 'absolute', left: '-40px', bottom: '-60px', display: 'flex', flexDirection: 'column'}}>
-                                                <img src={ArrowRight} width="60px"></img>
-                                                <p style={{margin: '0'}}>Klikk for mer info</p>
-                                            </div>
-                                        )}
-                                    */}
-                                </div>
-                            </Link>
-                            <Link to="/projects" className="grid-row-span-1">
-                                <article className="grid-box-2" /* onMouseOver={() => setShowArrow2(true)} onMouseOut={() => setShowArrow2(false)} */ >
-                                
-                                </article>
-                            </Link>
-                            <Link to="/expertise" className="grid-row-span-1">
-                                <article className="grid-box-3" /* onMouseOver={() => setShowArrow3(true)} onMouseOut={() => setShowArrow3(false)} */ >
-                                    
-                                </article>
-                            </Link>
-                        </section>
+                        <HomepageGrid />
                     </div>
                 </div>
                 <StarBackground />
