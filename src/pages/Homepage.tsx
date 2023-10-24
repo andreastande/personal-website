54018704aaf31fcf4aa1e8354673ebe604f3f882
andretan <andretan@stud.ntnu.no>
2023-10-24 20:55:18 +0200

    refactored homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import Header from '../components/Header';
 import StarBackground from '../components/StarBackground';
 import '../assets/Homepage.css'
@@ -8,61 +8,14 @@ import ArrowRight from '../assets/images/arrow-right.png'
 import PointerRight from '../assets/images/cursor-pointer-right.png'
 import PointerLeft from '../assets/images/cursor-pointer-left.png'
 
-const HomepageGrid = () => {
+
+const Homepage = () => {
+
     const [showArrow1, setShowArrow1] = useState(false);
     const [showArrow2, setShowArrow2] = useState(false);
     const [showArrow3, setShowArrow3] = useState(false);
 
-    return (
-        <section className="grid-container">
-            
-            <Link to="/about" className="grid-row-span-2">
-                <div style={{position: 'relative'}}>
-                    <article className="grid-box-1" onMouseOver={() => setShowArrow1(true)} onMouseOut={() => setShowArrow1(false)}>
-                        <img src={ProfilePicture}></img>
-                        <p>Mitt navn er Andreas Tande, jeg er 22 år og studerer Datateknologi ved NTNU. For øyeblikket er jeg på mitt tredje år.</p>
-                        <p id="additional-text-1">Klikk for å lese mer!</p>
-                    </article>
-                    {showArrow1 && (
-                        <div className="showMore" style={{position: 'absolute', left: '-40px', bottom: '-40px'}}>
-                            <img src={PointerRight} width="60px"></img>
-                        </div>
-                    )}
-                    
-                </div>
-            </Link>
-
-            <Link to="/projects" className="grid-row-span-1">
-                <div style={{position: 'relative'}}>
-                    <article className="grid-box-2" onMouseOver={() => setShowArrow2(true)} onMouseOut={() => setShowArrow2(false)}>
-                    
-                    </article>
-                    {showArrow2 && (
-                        <div className="showMore" style={{position: 'absolute'}}>
-                            <img src={PointerLeft} width="60px"></img>
-                        </div>
-                    )}
-                </div>
-            </Link>
-
-            <Link to="/expertise" className="grid-row-span-1">
-                <div style={{position: 'relative'}}>
-                    <article className="grid-box-3" onMouseOver={() => setShowArrow3(true)} onMouseOut={() => setShowArrow3(false)}>
-                        
-                    </article>
-                    {showArrow3 && (
-                        <div className="showMore" style={{position: 'absolute'}}>
-                            <img src={PointerLeft} width="60px"></img>
-                        </div>
-                    )}
-                </div>
-            </Link>
-
-        </section>
-    )
-}
-
-const Homepage = () => {
+    const backgroundComponent = useMemo(() => <StarBackground />, [])
     
     return (
         <div>
@@ -77,10 +30,54 @@ const Homepage = () => {
                         Her kan du få vite mer om meg, og oppdage prosjekter jeg har bidratt i eller utviklet selv. 
                     </p>
                     <div className="grid-container-wrapper">
-                        <HomepageGrid />
+                        <section className="grid-container">
+            
+                            <Link to="/about" className="grid-row-span-2">
+                                <div style={{position: 'relative'}}>
+                                    <article className="grid-box-1" onMouseOver={() => setShowArrow1(true)} onMouseOut={() => setShowArrow1(false)}>
+                                        <img src={ProfilePicture}></img>
+                                        <p>Mitt navn er Andreas Tande, jeg er 22 år og studerer Datateknologi ved NTNU. For øyeblikket er jeg på mitt tredje år.</p>
+                                        <p id="additional-text-1">Klikk for å lese mer!</p>
+                                    </article>
+                                    {showArrow1 && (
+                                        <div className="showMore" style={{position: 'absolute', left: '-40px', bottom: '-40px'}}>
+                                            <img src={PointerRight} width="60px"></img>
+                                        </div>
+                                    )}
+                                    
+                                </div>
+                            </Link>
+
+                            <Link to="/projects" className="grid-row-span-1">
+                                <div style={{position: 'relative'}}>
+                                    <article className="grid-box-2" onMouseOver={() => setShowArrow2(true)} onMouseOut={() => setShowArrow2(false)}>
+                                    
+                                    </article>
+                                    {showArrow2 && (
+                                        <div className="showMore" style={{position: 'absolute'}}>
+                                            <img src={PointerLeft} width="60px"></img>
+                                        </div>
+                                    )}
+                                </div>
+                            </Link>
+
+                            <Link to="/expertise" className="grid-row-span-1">
+                                <div style={{position: 'relative'}}>
+                                    <article className="grid-box-3" onMouseOver={() => setShowArrow3(true)} onMouseOut={() => setShowArrow3(false)}>
+                                        
+                                    </article>
+                                    {showArrow3 && (
+                                        <div className="showMore" style={{position: 'absolute'}}>
+                                            <img src={PointerLeft} width="60px"></img>
+                                        </div>
+                                    )}
+                                </div>
+                            </Link>
+
+                        </section>
                     </div>
                 </div>
-                <StarBackground />
+                {backgroundComponent}
             </main>
         </div>
     );
